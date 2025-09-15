15c49d3afd626856deba0a0721bee385d72de020
p.yazdanian <p.yazdanian74@gmail.com>
2025-09-15 13:04:01 -0400

    measure response time of llm-mas ollama

--- llm-mas-ec/scenario1-ollama/run.py
+++ llm-mas-ec/scenario1-ollama/run.py
@@ -7,7 +7,17 @@ from pymongo import MongoClient
 import json
 import time
 import re
+import logging
+import psutil
+import os
 
+logging.basicConfig(
+    filename='llm_mas_sc1_traces.log',  # Specify the log file name
+    level=logging.DEBUG,          # Set the logging level (e.g., DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s' # Define the log message format
+)
+
+process = psutil.Process(os.getpid())
 
 
 # 1. Define global state
@@ -157,6 +167,7 @@ def parse_json_response(content: str, fallback: dict):
         return fallback
     except Exception as e:
         print("JSON parse error:", e)
+        logging.error(f"JSON parse error: {e}")
         return fallback
 
 
@@ -169,10 +180,15 @@ def order_agent(state: OrderState, db_ag: DBAgent):
         order_id_in=state["order_id"] or str(uuid.uuid4()),
         status_in=state["status"] or "INIT"
                                  )
-    print(f"Order Agent: Sending prompt for LLM \n {prompt} \n ...")
+    logging.debug(f"Order Agent: Sending prompt for LLM \n {prompt} \n ...")
 
+    st = time.time()
     response = llm.invoke(prompt)
+    et = time.time()
+    logging.debug(f"Order Agent: LLM Raw Response Took {round((et - st), 3)} \n ...")
+
     print(f"Order Agent: LLM Raw Response Content is \n {response} \n ...")
+    logging.debug(f"Order Agent: LLM Raw Response Content is \n {response} \n ...")
 
     parsed = parse_json_response(
         response,
@@ -186,6 +202,7 @@ def order_agent(state: OrderState, db_ag: DBAgent):
     )
 
     print(f"Order Agent: Requested Qty: {state['qty']}, LLM Parsed Response: {parsed}, \n\n----------")
+    logging.debug(f"Order Agent: Requested Qty: {state['qty']}, LLM Parsed Response: {parsed}, \n\n----------")
 
     if parsed["status"] == "INIT":
         # Save INIT order
@@ -207,10 +224,15 @@ def inventory_agent(state: OrderState, db_ag: DBAgent):
         stock_in=stock
                                  )
 
-    print(f"Inventory Agent: Sending prompt for LLM \n {prompt} \n ...")
+    logging.debug(f"Inventory Agent: Sending prompt for LLM \n {prompt} \n ...")
 
+    st = time.time()
     response = llm.invoke(prompt)
+    et = time.time()
+    logging.debug(f"Inventory Agent: LLM Raw Response Took {round((et - st), 3)} \n ...")
+
     print(f"Inventory Agent: LLM Raw Response Content is \n {response} \n ...")
+    logging.debug(f"Inventory Agent: LLM Raw Response Content is \n {response} \n ...")
 
     parsed = parse_json_response(
         response,
@@ -218,6 +240,7 @@ def inventory_agent(state: OrderState, db_ag: DBAgent):
     )
 
     print(f"Inventory Agent: Current Stock: {stock}, Qty: {state['qty']}, LLM Parsed Response: {parsed}, \n\n----------")
+    logging.debug(f"Inventory Agent: Current Stock: {stock}, Qty: {state['qty']}, LLM Parsed Response: {parsed}, \n\n----------")
 
     # If reserved, decrement stock
     if parsed["status"] == "reserved":
@@ -257,6 +280,14 @@ def build_graph(db_ag: DBAgent):
 
 if __name__ == "__main__":
 
+    report_file_name = 'mas_sc1_parallel.txt'
+
+    cpu_start = process.cpu_times()
+    t1 = time.time()
+
+    with open(report_file_name, 'w') as f:
+        f.write('')
+
     db_mode = 'MOCK'  # REAL | MOCK
 
     if db_mode == 'REAL':
@@ -268,7 +299,7 @@ if __name__ == "__main__":
         db = None
     db_agent = DBAgent(db=db, mode=db_mode)
     graph = build_graph(db_agent)
-    delay = 1
+    delay = 0
 
     initial_state: OrderState = {"order_id": "", "item": "laptop", "qty": 2, "status": "INIT"}
 
@@ -277,4 +308,16 @@ if __name__ == "__main__":
     #    → OrderAgent (finalize order status)
     #    → END
     result = graph.invoke(initial_state)
-    print("Final state:", result)
+
+    t2 = time.time()
+    cpu_end = process.cpu_times()
+    cpu_used = (cpu_end.user - cpu_start.user) + (cpu_end.system - cpu_start.system)
+
+    logging.debug(f"Final state: {result}, Total Response Took: {round((t2 - t1), 3)}")
+    print("Final state:", result, f' Total Response Took: {round((t2 - t1), 3)}')
+    with open(report_file_name, 'a') as f1:
+        f1.write(f'Delay : {delay}, '
+                 f'Total Response Took: {round((t2 - t1), 3)}, '
+                 f'Status: {result}, '
+                 f'CPU time: {round(cpu_used, 5)} \n')
+
